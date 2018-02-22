3af0cc8c7646f2e33834ec31e7e3a3c7b9455c30
Matthew Mu <mucun1988@gmail.com>
2018-02-22 18:16:12 -0500

    correct-print

--- test.py
+++ test.py
@@ -10,15 +10,15 @@ make()  # compile the C code
 dim = 300
 lam = 1e-10
 
-printf('training sgns-qr model...')
+print('training sgns-qr model...')
 train_w2v_model(min_count=200, lam=lam, word_dim=dim, threads=12)
-printf('training sgns-qr model... done. \n')
+print('training sgns-qr model... done. \n')
 
-printf('load the result and evaluate the performance...')
+print('load the result and evaluate the performance...')
 vocab, inv_vocab, word_embedding = load_rslt_from_c_output()
 acc = analogical_reasoning(U=word_embedding,
                            vocab=vocab, inv_vocab=inv_vocab)
-printf('load the result and evaluate the performance... \n')
+print('load the result and evaluate the performance... \n')
 
 rslt = {'dim': dim, 'lam': lam, 'acc': acc['accuracy']}
 
