091818508ddd9ee88ed594d001cd1d2b0dfe3ba6
Matthew Mu <mucun1988@gmail.com>
2018-02-22 17:25:21 -0500

    update-test-file

--- test.py
+++ test.py
@@ -5,46 +5,18 @@ from scipy import stats
 from pyw2v_u import *
 import pickle as pk
 
-make()
+make()  # compile the C code
 
-num_iter = 5
-d_set = [900, 700, 500, 300, 100]
-lam_set = [1e-10, 1e-5, 1e-15, 0]
+dim = 300
+lam = 1e-10
 
 rslt = []
-for d in d_set:
-    for lam in lam_set:
-        print(f"d = {d}.")
-        train_w2v_model(num_iter=num_iter, min_count=200,
-                        lam=lam, threads=12, word_dim=d)
-        vocab, inv_vocab, word_embedding = load_rslt_from_c_output()
 
-        # analogy
-        acc = analogical_reasoning(U=word_embedding,
-                                   vocab=vocab, inv_vocab=inv_vocab)
-        rslt.append({'d': d, 'lam': lam, 'acc': acc['accuracy']})
+train_w2v_model(min_count=200, lam=lam, word_dim=d)
+vocab, inv_vocab, word_embedding = load_rslt_from_c_output()
 
-        df_rslt = pd.DataFrame(rslt)
-        print(df_rslt)
-        output_file = 'rslt/test_final_submit.csv'
-        df_rslt.to_csv(output_file, index=False)
+# analogy
+acc = analogical_reasoning(U=word_embedding,
+                           vocab=vocab, inv_vocab=inv_vocab)
 
-# 10: update once (does not work)
-# 11: update more times (plausible)
-# 12: fix lambda + update more times ()
-# 13: chnage parameter settings: k = 15, downsample=1e-5  (plausible)
-# 14: more iterations 5 --> 20 (finally try more iterations + 15 seems bad)
-# 15: update once after one epoch (plausible)
-# 16: U, V initialized both as normal same (marginally, lamba = 10^-10)
-# 17: what others (wired initializations)
-# 18: sqrt initializations
-# 19: larger than 200 (not good enough)
-# 20: longer step size (how)
-
-# linux: /iter + fixed lambda (not good)
-# 30: decreasing lambda (good) + normal method w.o, num_thread
-# 40: /num_thread (doing)
-# 50: method 4
-# 504:
-
-# method 3 not working well
+print({'dim': dim, 'lam': lam, 'acc': acc['accuracy']})
